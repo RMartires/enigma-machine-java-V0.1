60a419d1627c7a664198e0ce73ec07835696f6d9
R()#!t_c()d3s <30775934+l0ve2c0de@users.noreply.github.com>
2018-12-24 02:00:34 +0530

    First commit -no plugboard

---
+++ src/enigma/machine/EnigmaMachine.java
@@ -0,0 +1,20 @@
+package enigma.machine;
+
+import java.awt.*;
+import javax.swing.*;
+import java.util.ArrayList;
+
+
+public class EnigmaMachine {
+    
+    public static void main(String[] args) {
+    
+       Screen.CreateScreen();
+       
+       
+        
+    }
+    
+        
+}
+
